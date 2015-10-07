a6fc7cd0f1e0ac399c29fea89ac96ff636e60d14
Mitchell,Renee <mit12020@byui.edu>
2015-10-06 20:25:15 -0400

    10/5/15 - Practice commit to repository

--- src/thehurricanesurvivalgame/TheHurricaneSurvivalGame.java
+++ src/thehurricanesurvivalgame/TheHurricaneSurvivalGame.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package moonlight;
+package thehurricanesurvivalgame;
 
 /**
  *
  * @author Renee
  */
-public class MoonLight {
+public class TheHurricaneSurvivalGame {
 
     /**
      * @param args the command line arguments
