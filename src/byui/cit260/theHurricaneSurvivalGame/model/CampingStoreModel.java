9218596ed44a4a3a9216228a11df4c25ff604e06
Mitchell,Renee <mit12020@byui.edu>
2015-10-08 19:41:01 -0400

    10/5/15 - Practice commit to repository 10/8/15 - CampingStoreModel commit to repository

--- src/byui/cit260/theHurricaneSurvivalGame/model/CampingStoreModel.java
+++ src/byui/cit260/theHurricaneSurvivalGame/model/CampingStoreModel.java
@@ -5,10 +5,115 @@
  */
 package byui.cit260.theHurricaneSurvivalGame.model;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  *
  * @author Renee
  */
-public class CampingStoreModel {
+public class CampingStoreModel implements Serializable {
+    
+    //class instance variables
+    private String tentAndCanopyAisle;
+    private String lightingAisle;
+    private String cookingSupplyAisle;
+    private String gearAisle;
+    private String generatorsAisle;
+
+    public CampingStoreModel() {
+    }
+    
+    
+
+    public String getTentAndCanopyAisle() {
+        return tentAndCanopyAisle;
+    }
+
+    public void setTentAndCanopyAisle(String tentAndCanopyAisle) {
+        this.tentAndCanopyAisle = tentAndCanopyAisle;
+    }
+
+    public String getLightingAisle() {
+        return lightingAisle;
+    }
+
+    public void setLightingAisle(String lightingAisle) {
+        this.lightingAisle = lightingAisle;
+    }
+
+    public String getCookingSupplyAisle() {
+        return cookingSupplyAisle;
+    }
+
+    public void setCookingSupplyAisle(String cookingSupplyAisle) {
+        this.cookingSupplyAisle = cookingSupplyAisle;
+    }
+
+    public String getGearAisle() {
+        return gearAisle;
+    }
+
+    public void setGearAisle(String gearAisle) {
+        this.gearAisle = gearAisle;
+    }
+
+    public String getGeneratorsAisle() {
+        return generatorsAisle;
+    }
+
+    public void setGeneratorsAisle(String generatorsAisle) {
+        this.generatorsAisle = generatorsAisle;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 43 * hash + Objects.hashCode(this.tentAndCanopyAisle);
+        hash = 43 * hash + Objects.hashCode(this.lightingAisle);
+        hash = 43 * hash + Objects.hashCode(this.cookingSupplyAisle);
+        hash = 43 * hash + Objects.hashCode(this.gearAisle);
+        hash = 43 * hash + Objects.hashCode(this.generatorsAisle);
+        return hash;
+        
+        
+    }
+
+    @Override
+    public String toString() {
+        return "CampingStoreModel{" + "tentAndCanopyAisle=" + tentAndCanopyAisle + ", lightingAisle=" + lightingAisle + ", cookingSupplyAisle=" + cookingSupplyAisle + ", gearAisle=" + gearAisle + ", generatorsAisle=" + generatorsAisle + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final CampingStoreModel other = (CampingStoreModel) obj;
+        if (!Objects.equals(this.tentAndCanopyAisle, other.tentAndCanopyAisle)) {
+            return false;
+        }
+        if (!Objects.equals(this.lightingAisle, other.lightingAisle)) {
+            return false;
+        }
+        if (!Objects.equals(this.cookingSupplyAisle, other.cookingSupplyAisle)) {
+            return false;
+        }
+        if (!Objects.equals(this.gearAisle, other.gearAisle)) {
+            return false;
+        }
+        if (!Objects.equals(this.generatorsAisle, other.generatorsAisle)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
+    
     
 }
--- src/thehurricanesurvivalgame/TheHurricaneSurvivalGame.java
+++ src/thehurricanesurvivalgame/TheHurricaneSurvivalGame.java
@@ -1,6 +1,7 @@
 
 package thehurricanesurvivalgame;
 
+import byui.cit260.theHurricaneSurvivalGame.model.CampingStoreModel;
 import byui.cit260.theHurricaneSurvivalGame.model.MapModel;
 
 /**
@@ -13,13 +14,29 @@ public class TheHurricaneSurvivalGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-            MapModel mainMap = new MapModel();
+        MapModel mainMap = new MapModel();
             
-            mainMap.setLocation("House");
-            mainMap.setCoordinates(7);
+        mainMap.setLocation("House");
+        mainMap.setCoordinates(7);
             
-            String mapInfo = mainMap.toString();
-            System.out.println(mainMap);
+        String mapInfo = mainMap.toString();
+        System.out.println(mainMap);
+        
+        
+        CampingStoreModel department = new CampingStoreModel();
+        
+        department.setTentAndCanopyAisle("Aisle 2");
+        department.setLightingAisle("Aisle 5");
+        department.setCookingSupplyAisle("Aisle 1");
+        department.setGearAisle("Aisle 4");
+        department.setGeneratorsAisle("Aisle 3");
+        
+        String campingStoreInfo = department.toString();
+        System.out.println(campingStoreInfo);
+        
+        
+        
+         }  
     }
     
-}
+    
