20f9beebfa842afa8414abe6814322e24b1568b9
SteffanyFaldmo <SteffanyFaldmo@thecreativeruby.home>
2015-10-24 20:33:22 -0700

    STEFF - Individual Assignment Classes Added 10/10 STEFF - Individual Assignment timeControl function 10/24

--- src/byui/cit260/theHurricaneSurvivalGame/control/timeControl.java
+++ src/byui/cit260/theHurricaneSurvivalGame/control/timeControl.java
@@ -7,8 +7,23 @@ package byui.cit260.theHurricaneSurvivalGame.control;
 
 /**
  *
- * @author Renee
+ * @author Steffany
  */
 public class timeControl {
-    
+        /**
+    *<h2>Time:</h2>
+    *<p>Calculate the time left in the game.</p>
+    *@param TimeSpent is the time spent in game
+    *@param TotalTime is the total time 
+    *@return What is the time remaining?
+    */
+     public double calcTimeRemainingInGame(double TimeSpent, double TotalTime){
+         if (TotalTime < 48) { // TotalTime must always be 48 hours 
+             return 0;
+        }
+         
+         double TimeRemaining = TotalTime - TimeSpent;
+
+        return TimeRemaining;
+    }
 }
--- src/byui/cit260/theHurricaneSurvivalGame/control/viewControl.java
+++ src/byui/cit260/theHurricaneSurvivalGame/control/viewControl.java
@@ -7,7 +7,7 @@ package byui.cit260.theHurricaneSurvivalGame.control;
 
 /**
  *
- * @author Renee
+ * @author Steffany
  */
 public class viewControl {
     
