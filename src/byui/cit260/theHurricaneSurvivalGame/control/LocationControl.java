f289892ddbbb389841dc28b44a4248a22a49fb91
Mitchell,Renee <mit12020@byui.edu>
2015-12-02 19:04:32 -0500

    Wed night 1st commit

--- src/byui/cit260/theHurricaneSurvivalGame/control/LocationControl.java
+++ src/byui/cit260/theHurricaneSurvivalGame/control/LocationControl.java
@@ -19,48 +19,45 @@ public class LocationControl {
 
     public boolean moveNorth(Player p, Map m) throws MoveException, MoveException {
 
-      if (p.getLocation().getRow() == 0) {
-            throw new MoveException("Player cannot move leave the map");
-        }
+      if(p.getLocation().getRow() == 0){
+            throw new MoveException("Player cannot move from the map");
+      
 
         Location newLoc = m.getMatrix()[p.getLocation().getRow() - 1][p.getLocation().getCol()]{
 
         p.setPlayerLocation(newLoc);
+        
 
-        return true;
-    }
-}
-    public boolean  moveEast(Player p, Map m) throws MoveException, MoveException {
+    public boolean moveEast(Player p, Map m) throws MoveException, MoveException {
+        
+   
         if(p.getLocation().getRow() == 0){
             throw new MoveException("Player cannot move from the map");
             
        
-        Location newLoc = m.getMatrix()[p.getLocation().getRow() -1][p.getLocation().getCol()]{
+        Location newLoc = m.getMatrix()[p.getLocation().getRow() -1][p.getLocation().getCol()];
         
          p.setPlayerLocation(newLoc);
         
-        return true;
-    }
-
-
-    
+     
+        
     public boolean moveSouth(Player p, Map m)throws MoveException{
         if(p.getLocation().getRow() == 0){
-            throw new MoveException("Player can move from current location");
+            throw new MoveException("Player can move from the map");
        
         Location newLoc = m.getMatrix()[p.getLocation().getRow() -1][p.getLocation().getCol()]{
         
         p.setPlayerLocation(newLoc);
         
-        return true;
+       
     
 
       public boolean moveWest(Player p, Map m)throws MoveException{
         if(p.getLocation().getRow() == 1){
-            throw new MoveException("Player can move from current location");
+            throw new MoveException("Player can move from the map");
      
         Location newLoc = null m.getMatrix()[p.getLocation().getRow() -1][p.getLocation().getCol()]{
         p.setPlayerLocation(newLoc);
         
-        return true;
+
 
